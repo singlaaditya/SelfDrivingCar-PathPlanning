19424a7a004760ac7d22dcc6176279a51e691aa3
singlaaditya <adityasingla87@gmail.com>
2021-05-14 21:56:34 +0000

    Add logic for collision avoidance and reduce jerk

--- src/main.cpp
+++ src/main.cpp
@@ -52,16 +52,18 @@ int main() {
     map_waypoints_dy.push_back(d_y);
   }
   
-int lane = 1;
-double ref_vel = 49.5;
+ int lane = 1;
+  double ref_vel = 0;
 
-  h.onMessage([&ref_vel, &lane, &map_waypoints_x,&map_waypoints_y,&map_waypoints_s,
+  h.onMessage([&lane, &ref_vel, &map_waypoints_x,&map_waypoints_y,&map_waypoints_s,
                &map_waypoints_dx,&map_waypoints_dy]
               (uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length,
                uWS::OpCode opCode) {
     // "42" at the start of the message means there's a websocket message event.
     // The 4 signifies a websocket message
     // The 2 signifies a websocket event
+                
+
     if (length && length > 2 && data[0] == '4' && data[1] == '2') {
 
       auto s = hasData(data);
@@ -97,6 +99,42 @@ double ref_vel = 49.5;
  
  
           json msgJson;
+          if (prev_size > 0)
+		  {
+			car_s = end_path_s;
+		  }
+		  
+		  bool too_close = false;
+		  
+		  for (int i =0; i < sensor_fusion.size(); i++)
+		  {
+			float d = sensor_fusion[i][6];
+			if (d < (4* (lane +1)) && d > (4 *lane))
+			{
+				double vx = sensor_fusion[i][3];
+				double vy = sensor_fusion[i][4];
+				double check_speed = sqrt(vx*vx+vy*vy);
+				double check_car_s = sensor_fusion[i][5];
+				check_car_s += ((double)prev_size*0.02*check_speed);
+				
+				if ((check_car_s>car_s) && (check_car_s - car_s) < 30)
+                {
+					too_close = true;
+                  break;
+				}
+				
+			}
+		   }
+		   
+		   if(too_close)
+		   {
+			ref_vel -= 0.224;
+		    }
+			else if (ref_vel < 49.5)
+			{
+				ref_vel += 0.224;
+			}
+
 
           vector<double> next_x_vals;
           vector<double> next_y_vals;
@@ -117,8 +155,8 @@ double ref_yaw = deg2rad(car_yaw);
 //if the previous path contains one or zero points then we cant use previous path since we need two points.
 if(prev_size < 2)
 {
-	double prev_car_x = car_x - cos(car_yaw);
-	double prev_car_y = car_y - sin(car_yaw);
+	double prev_car_x = car_x - cos(ref_yaw);
+	double prev_car_y = car_y - sin(ref_yaw);
 	
 	
      ptsx.push_back(prev_car_x);
